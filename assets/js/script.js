eaac69f4b5b01861ca8d8e023cf3335bbf31eca7
Roswil Ascanio <ascanioroswil@gmail.com>
2022-01-25 23:29:45 -0300

    se agrega  cambios en el css

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,25 @@ $(window).scroll(function(){
       }
 
 });
+
+$(document).ready(function () {
+
+
+    $("a").click(function(event){
+        if (this.hash !== "") {
+          event.preventDefault();
+    
+          var gato = this.hash;
+    
+          $("html, body").animate({
+            scrollTop: $(gato).offset().top - 70
+          }, 1000, );
+        }
+    });  
+
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
